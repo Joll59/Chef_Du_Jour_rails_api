ea66dc31b405114b8e61949ed96ce1bd775c85c7
Yomi <alajide@gmail.com>
2017-02-13 17:24:02 -0500

    modify user migration table and add user model validation comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+
+  # validates :name, presence: true, length: { maximum: 50 }
+  #   validates :address, presence: true, length: { maximum: 100 }
+  #   validates :phone, length: { is: 10 }
+  #   validates :user_id, uniqueness: true
 end
--- db/migrate/20170213203432_create_users.rb
+++ db/migrate/20170213203432_create_users.rb
@@ -1,8 +1,17 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :first_name
+      t.string :last_name
+      t.string :phone_number
       t.string :email
+      t.string :street1
+      t.string :street2
+      t.string :city
+      t.string :state
+      t.string :zipcode
+      t.string :profile_pic_url
+      t.integer :average_user_rating
       t.string :password_digest
 
       t.timestamps

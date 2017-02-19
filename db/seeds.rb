a3f20925a9819c4a3cc10d7b51e1bbd3a9e9278c
Rob Hitt <robhitt@gmail.com>
2017-02-19 13:52:35 -0500

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ User.create(first_name:  "Josh", last_name: "Pann", email: "josh.pann@gmail.com"
 User.create(first_name: "Bob", last_name: "Candy", email: "bob.cany@gmail.com", password: "1234", phone_number: "444-444-4444", street1: "allen", street2: "apt 2b", city: "New York", state: "NY", zipcode: "10003", chef_biography: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum facilisis leo vel auctor. Duis vitae consequat tortor. Donec quis sapien condimentum, imperdiet urna ac, viverra purus. In fermentum pretium ultricies. Curabitur non magna non elit dapibus elementum quis eleifend magna. Nullam et sagittis neque. Nulla auctor erat sed ex.", instagram:"", personal_website: "www.yahoo.com")
 User.create(first_name: "Madonna", last_name: "Smith", email: "madonna@gmail.com", password: "1234", phone_number: "333-333-3333", street1: "broadway", street2: "apt 1c", city: "New York", state: "NY", zipcode: "10000", chef_biography: "", instagram:"", personal_website:"" )
 User.create(first_name: "Kim", last_name: "West", email: "kim@gmail.com", password: "1234", phone_number: "222-222-2222", street1: "stanton", street2: "apt 8f", city: "New York", state: "NY", zipcode: "10005", chef_biography: "", instagram:"", personal_website:"")
+User.create(first_name: "Rob", last_name: "Hitt", email: "robhitt@gmail.com", password: "1234", phone_number: "732-682-4952", street1: "214 N. 11th St.", street2: "Apt 3V", city: "Brooklyn", state: "NY", zipcode: "11211", chef_biography: "I'm the culinary king, graduated Harvard culinary school with my PH-Delicious", instagram:"@robhitt", personal_website:"www.robhitt.com")
 
 
 # DiningExperience data
@@ -10,6 +11,7 @@ DiningExperience.create(title: "Dinner in Tuscany", description: "Lorem ipsum do
 DiningExperience.create(title: "Romantic Date Night", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non diam tristique, sodales ante non, auctor dui. Suspendisse nulla tortor.", price: 200, length: 3, user_id: 1)
 #Created by User 2 (Bob)
 DiningExperience.create(title: "Seafood for Two", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sagittis nisl eu est luctus tempor. Nulla cursus varius fermentum. Phasellus.", price: 50, length: 1, user_id: 2)
+DiningExperience.create(title: "Desert Feast for Two", description: "Enjoy the on fire Baked Alaska along with Thin Mints", price: 50, length: 4, user_id: 5)
 
 #Reservations data
 
@@ -18,5 +20,6 @@ Reservation.create(dining_experience_id: 2, user_id: 3, date: 02/21/2017)
 Reservation.create(dining_experience_id: 2, user_id: 2, date: 02/22/2017)
 Reservation.create(dining_experience_id: 3, user_id: 1, date: 02/25/2017)
 Reservation.create(dining_experience_id: 3, user_id: 3, date: 02/24/2017)
+Reservation.create(dining_experience_id: 4, user_id: 5, date: 02/24/2017)
 
 #mm/dd/yyyy

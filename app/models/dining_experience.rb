17431b6c64623e0a4fd1bfbb68d92334460805eb
Rob Hitt <robhitt@gmail.com>
2017-02-21 17:19:05 -0500

    add dining experience edit and navbar jwt fix

--- app/models/dining_experience.rb
+++ app/models/dining_experience.rb
@@ -1,5 +1,6 @@
 class DiningExperience < ApplicationRecord
   has_many :reservations
+  has_many :signature_dishes
   #DiningExperience.First.users
   #returns all users (think consumers) who have reserved this expeirence
   has_many :users,  through: :reservations
@@ -7,8 +8,6 @@ class DiningExperience < ApplicationRecord
   #DiningExperience.First.user
   #returns the user who created this experience
   belongs_to :user
-
-
 end
 
 
--- app/serializers/dining_experience_serializer.rb
+++ app/serializers/dining_experience_serializer.rb
@@ -2,4 +2,5 @@ class DiningExperienceSerializer < ActiveModel::Serializer
   attributes :id, :description, :price, :length, :title
   belongs_to :user
   has_many :reservations
+  has_many :signature_dishes
 end
